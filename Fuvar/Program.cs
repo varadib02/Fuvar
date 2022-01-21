415e6b4d0d284daf6f9a23d3d5bdc42d230b9e98
varadib02 <barkabas02@gmail.com>
2022-01-21 10:20:20 +0100

    Add project files.

---
+++ Fuvar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fuvar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<taxi> taxik = new List<taxi>();
+            foreach (var sor in File.ReadAllLines("fuvar.csv").Skip(1)) taxik.Add(new taxi(sor));
+        }
+    }
+}
---
+++ Fuvar/taxi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuvar
+{
+    class taxi
+    {
+        public int azon { get; set; }
+        public DateTime indulás { get; set; }
+        public int idotartam { get; set; }
+        public double tav { get; set; }
+        public double viteldij { get; set; }
+        public double borravalo { get; set; }
+        public string fizetesmod { get; set; }
+        public taxi(string sor)
+        {
+            string[] db = sor.Split(';');
+            azon = int.Parse(db[0]);
+            indulás = DateTime.Parse(db[1]);
+            idotartam = int.Parse(db[2]);
+            tav = double.Parse(db[3]);
+            viteldij = double.Parse(db[4]);
+            borravalo = double.Parse(db[5]);
+            fizetesmod = db[6];
+            
+        }
+    }
+}
