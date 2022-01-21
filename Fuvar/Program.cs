6a0c9aa96ad3ae40697d31bf65fc313e648bfbac
varadib02 <barkabas02@gmail.com>
2022-01-21 10:34:36 +0100

    4.f

--- Fuvar/Program.cs
+++ Fuvar/Program.cs
@@ -14,6 +14,8 @@ namespace Fuvar
             List<taxi> taxik = new List<taxi>();
             foreach (var sor in File.ReadAllLines("fuvar.csv").Skip(1)) taxik.Add(new taxi(sor));
             Console.WriteLine($"3.feladat: {taxik.Count} fuvar");
+            Console.WriteLine($"4.feladat: {taxik.Where(x => x.azon == 6185).Count()} fuvar alatt: {taxik.Where(x=>x.azon==6185).Sum(x=>x.viteldij+x.borravalo)}$");
+            Console.WriteLine($"");
         }
     }
 }
